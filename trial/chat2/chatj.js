5698a5deba184b3a4053759e71339763d1792b89
DoItNowokay <120119721+DoItNowokay@users.noreply.github.com>
2024-02-12 10:10:43 +0530

    api key removed

--- trial/chat2/chatj.js
+++ trial/chat2/chatj.js
@@ -1,7 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI("AIzaSyAzrTxXGz0bx3KCxdQziTGhkUU7qRsEkdA");
+const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
 
 async function run(question) {
   // For text-only input, use the gemini-pro model
